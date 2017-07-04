b0e66b8e90cbcf627470b3b93759d38910128f9f
Kelvin Sherlock <ksherlock@gmail.com>
2017-07-04 01:07:08 -0400

    add ksherlock/lemonxx formula

---
+++ lemonxx.rb
@@ -0,0 +1,30 @@
+
+class Lemonxx < Formula
+  desc "lemon parser generator with improved c++ support"
+  homepage "https://github.com/ksherlock/lemonxx/"
+  url "https://github.com/ksherlock/lemonxx/archive/2017-07-03.tar.gz"
+  sha256 "36f4f17d76d94b9ebaafca036528922b3bda9013d8a12fbd782065171d345a50"
+
+  head do
+    url "https://github.com/ksherlock/lemonxx.git"
+  end
+
+  def install
+    (share/"lemon").install "lempar.c"
+    (share/"lemon").install "lempar.cpp"
+    (share/"lemon").install "lempar.cxx"
+    include.install "lemon_base.h"
+
+    system "make HOMEBREW_TEMPLATE_PATH=#{share}/lemon/"
+    bin.install "lemon"
+    bin.install "lemon++"
+    bin.install "lemon--"
+  end
+
+  conflicts_with "lemon", :because => "both install `lemon` binaries"
+
+  test do
+    # system "false"
+    system "true"
+  end
+end
